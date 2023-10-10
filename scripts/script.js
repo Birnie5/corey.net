6c4696f41caf290692397a58dd1ff56b75910950
Birnie5 <38158468+Birnie5@users.noreply.github.com>
2023-10-11 00:06:13 +0100

    Website
    fixed layout issues and cleaned css

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
+// scripts/yourExistingScript.js
+
 const currentYear = new Date().getFullYear();
 document.getElementById('copyright').textContent = `© ${currentYear} Corey Birnie`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".profile-pic");
+    const zoomOverlay = document.createElement("div");
+    const zoomedImage = document.createElement("img");
+
+    zoomOverlay.className = 'zoom-overlay';
+    zoomOverlay.appendChild(zoomedImage);
+    document.body.appendChild(zoomOverlay);
+
+    images.forEach(image => {
+        image.addEventListener("click", () => {
+            zoomedImage.src = image.src;
+            zoomOverlay.style.display = "flex";
+        });
+    });
+
+    zoomOverlay.addEventListener("click", () => {
+        zoomOverlay.style.display = "none";
+    });
+});
